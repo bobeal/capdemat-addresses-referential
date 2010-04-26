46a4862250e00c435518c4d32088659201975b1e
Sylvain MATHIEU <sma@zenexity.fr>
2010-04-26 11:23:20 +0200

    import - adjustments

--- app/controllers/Import.java
+++ app/controllers/Import.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceException;
 import models.Way;
 import models.City;
 import play.Logger;
@@ -14,31 +16,31 @@ import play.mvc.Controller;
 public class Import extends Controller {
 
     public static void mediapost() throws FileNotFoundException, IOException {
-        City.deleteAll();
         Way.deleteAll();
+        City.deleteAll();
 
         File mediapostFile = Play.getFile("data/hffvnn85.tri");
         BufferedReader  mediapostBr = new BufferedReader(new FileReader(mediapostFile));
         String line;
-        int i = 0;
+        int i = 0, ignoredCities = 0;
+        City currentCity = null;
         while((line = mediapostBr.readLine()) != null) {
-            City currentCity = null;
 
             // Voies
-            if(line.substring(0, 1).equals("V")) {
+            if(currentCity != null && line.substring(0, 1).equals("V")) {
                 Way way = new Way();
                 way.city = currentCity;
-                way.matriculation = line.substring(7, 15);
+                way.matriculation = line.substring(6, 14);
                 way.name = line.substring(54, 86).trim();
                 way.save();
             }
 
             // Voies synonymes (ancienne appellation ou appellation locale)
-            else if(line.substring(0, 1).equals("W")) {
+            else if(currentCity != null && line.substring(0, 1).equals("W")) {
                 Way way = new Way();
                 way.city = currentCity;
-                way.synonym = line.substring(6,15);
-                way.matriculation = line.substring(15, 22);
+                way.synonym = line.substring(6, 14);
+                way.matriculation = line.substring(14, 22);
                 way.name = line.substring(54, 86).trim();
                 way.save();
             }
@@ -46,11 +48,17 @@ public class Import extends Controller {
             // Localité
             else if(line.substring(0, 1).equals("L")) {
                 City.em().clear();
-                currentCity = new City();
-                currentCity.inseeCode = line.substring(1, 6);
-                currentCity.postalCode = line.substring(93, 99);
-                currentCity.name = line.substring(39, 71).trim();
-                currentCity.save();
+                String inseeCode = line.substring(1, 6);
+                if(currentCity == null || currentCity != null && !inseeCode.equals(currentCity.inseeCode)) {
+                    currentCity = new City();
+                    currentCity.inseeCode = inseeCode;
+                    currentCity.postalCode = line.substring(93, 98);
+                    currentCity.name = line.substring(39, 71).trim();
+                    currentCity.save();
+                }
+                else {
+                    ignoredCities++;
+                }
             }
 
             i++;
@@ -58,6 +66,7 @@ public class Import extends Controller {
                 Logger.info("%d processed rows...", i);
             }
         }
+        Logger.info("%s ignored cities", ignoredCities);
         renderText("ok");
     }
 
