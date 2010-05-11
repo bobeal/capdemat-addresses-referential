23e4b461d886647db97208fc4a369ddbee004078
Sylvain MATHIEU <sma@zenexity.fr>
2010-05-11 16:08:22 +0200

    Implemented WS security with expiration date

--- app/controllers/Services.java
+++ app/controllers/Services.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import java.util.Date;
 import models.AccessControl;
 import play.Logger;
 import play.mvc.Before;
@@ -16,7 +17,7 @@ public class Services extends Controller{
             renderJSON("{message: \"The parameter 'token' is required.\"}");
         }
         Logger.debug("Token: %s, IP: %s", token, IP);
-        AccessControl ac = AccessControl.find("IP=? and token=?", IP, token).first();
+        AccessControl ac = AccessControl.find("IP=? and token=? and expirationDate > ?", IP, token, new Date()).first();
         if(ac==null) {
             response.status = 400;
             renderJSON("{message: \"The token is not valid for this IP.\"}");
