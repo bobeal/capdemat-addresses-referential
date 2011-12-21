a461708e81b6bac64adb4cd1ad0709c4fd7bae11
Damien Boissin <dbo@zenexity.com>
2011-12-21 15:40:42 +0100

    [Fix] Issue #1066 : Add cascade on foreign key constraints.

--- db/update_V1_1.sql
+++ db/update_V1_1.sql
@@ -1,2 +1,8 @@
+alter table city drop constraint fk200d8b88194376;
+alter table way drop constraint fk152cf88194376;
+
+alter table city add constraint fk_referential_city foreign key (referential_id) references referential (id) match simple on update cascade on delete cascade;
+alter table way add constraint fk_referential_way foreign key (referential_id) references referential (id) match simple on update cascade on delete cascade;
+
 alter table importlog add line bigint;
 
