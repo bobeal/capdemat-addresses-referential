2f781f48588546d36015dc02035e5428ac813ca3
Damien Boissin <dbo@zenexity.com>
2011-12-21 17:31:53 +0100

    a461708e81b6bac64adb4cd1ad0709c4fd7bae11

--- db/update_V1_1.sql
+++ db/update_V1_1.sql
@@ -1,8 +1,18 @@
 alter table city drop constraint fk200d8b88194376;
 alter table way drop constraint fk152cf88194376;
+alter table import drop constraint fk82c65fc588194376;
+alter table accesscontrol drop constraint fk5f375f3988194376;
+alter table importlog drop constraint fk6b03f93f320ca4bb;
+alter table accesscontrol drop constraint fk5f375f3955fd001e;
+alter table expirationnotification drop constraint fk9e96455ad12fe56;
 
 alter table city add constraint fk_referential_city foreign key (referential_id) references referential (id) match simple on update cascade on delete cascade;
 alter table way add constraint fk_referential_way foreign key (referential_id) references referential (id) match simple on update cascade on delete cascade;
+alter table import add constraint fk_referential_import foreign key (referential_id) references referential (id) match simple on update cascade on delete cascade;
+alter table accesscontrol add constraint fk_referential_accesscontrol foreign key (referential_id) references referential (id) match simple on update cascade on delete cascade;
+alter table importlog add constraint fk_referential_importlog foreign key (referential_id) references referential (id) match simple on update cascade on delete cascade;
+alter table accesscontrol add constraint fk_customer_accesscontrol foreign key (customer_id) references customer (id) match simple on update cascade on delete cascade;
+alter table expirationnotification add constraint fk_accesscontrol_expirationnotification foreign key (accesscontrol_id) references accesscontrol (id) match simple on update cascade on delete cascade;
 
 alter table importlog add line bigint;
 
